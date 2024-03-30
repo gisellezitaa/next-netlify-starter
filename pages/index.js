f4052afa02aba7b3005fe4dc85fd88cbd546d272
gisellezitaa <160813436+gisellezitaa@users.noreply.github.com>
2024-03-29 23:52:01 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          oLHA elu sexo sem para
         </p>
       </main>
 
